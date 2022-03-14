77b7c93b9134a82cba3a011640d89f024b74cd24
muvvamadhuri <muvva.m@northeastern.edu>
2022-03-13 23:50:47 -0400

    Create question4.sh

---
+++ question4.sh
@@ -0,0 +1,13 @@
+#! /bin/bash
+#install putty using
+sudo apt install putty
+#on the server side, install openssh-server
+#sudo apt-get install openssl-server
+#above thing is to be done on the server side
+
+#open putty
+putty
+
+
+#! /bin/bash
+sudo reboot
